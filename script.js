240e1a3df4bf0d9b608cc0794f52bac14aefe4df
santosh <yadavsantosh6720@gmail.com>
2022-09-18 13:09:16 +0530

    Typing Changes

---
+++ script.js
@@ -0,0 +1,8 @@
+// *********************** ********************** Typing Animation ***************************** **************
+
+var typed = new Typed(".typing",{
+    strings:["Problem Solver","Aspiring Software Developer","Aspiring Full Stack Developer","Very Enthusiastic"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+})
